0d3e4d9d2ef2c4af4b8c84b35d58ace5b47b0598
Sebastian Cloos Hylander <sehy@itu.dk>
2023-08-31 15:23:29 +0200

    Functions for read and cheep for easy seperation
    Co-authored-by: Casper<ccaassppeerr2411@gmail.com>
    Co-authored-by: Line<linejkpraestegaard@gmail.com>
    Co-authored-by: Max<max@qbrix.dk>
    Co-authored-by: Daniel<danielfich2@gmail.com>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+using System.Threading.Channels;
+
+class Program{
+    public static void Main(string[] args){
+        if(args.Length == 0){
+            Console.WriteLine("Please choose 'read' or 'cheep'");
+            return;
+        }
+
+        if(args[0] == "read"){
+            read();
+        }
+        else if(args[0] == "cheep"){
+            cheep(args[1]);
+        }
+    }
+
+    private static void cheep(string message){
+
+    }
+
+    private static void read(){
+
+    }
+}
