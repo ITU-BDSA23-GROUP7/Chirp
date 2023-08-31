d46dfb271c0afab5ff19b5328def5a737247be25
Sebastian Cloos Hylander <sehy@itu.dk>
2023-08-31 15:49:04 +0200

    edited cheep method to write to the csv file
    Co-authored-by: Casper<ccaassppeerr2411@gmail.com>
    Co-authored-by: Line<linejkpraestegaard@gmail.com>
    Co-authored-by: Max<max@qbrix.dk>
    Co-authored-by: Daniel<danielfich2@gmail.com>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,26 +1,39 @@
-﻿using System.ComponentModel.Design;
-using System.Threading.Channels;
+﻿using System;
+using System.IO;
 
-class Program{
-    public static void Main(string[] args){
-        if(args.Length == 0){
+class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
             Console.WriteLine("Please choose 'read' or 'cheep'");
             return;
         }
 
-        if(args[0] == "read"){
+        if (args[0] == "read")
+        {
             read();
         }
-        else if(args[0] == "cheep"){
+        else if (args[0] == "cheep")
+        {
             cheep(args[1]);
         }
     }
 
-    private static void cheep(string message){
+    private static void read()
+    {
 
     }
 
-    private static void read(){
+    private static void cheep(string message)
+    {
+        using (var writer = new StreamWriter("chirp_cli_db.csv",true))
+        {
+            string author = "anon";
+            var timestamp = "100048";
+            writer.WriteLine($"{author},{timestamp},{message}");
+        }
 
     }
 }
