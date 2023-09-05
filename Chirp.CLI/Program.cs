23adc67f3fd32179bf72815d3a6b5f1ba9d20a88
Sebastian Cloos Hylander <sehy@itu.dk>
2023-09-05 12:57:52 +0200

    Made the 'read' method with exception handling
    Co-authored-by: Line<linejkpraestegaard@gmail.com>
    Co-authored-by: Daniel<danielfich2@gmail.com>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -23,7 +23,27 @@ class Program
 
     private static void read()
     {
+        using (var reader = new StreamReader("chirp_cli_db.csv")){
 
+            // reads next line throws NullReferenceException if it returns 'null'
+            string? nextln = reader.ReadLine() ?? throw new NullReferenceException();
+            string[] arr = nextln.Split(",");
+
+            if(!(arr.Length == 3 && arr[0] == "Author" && arr[1] == "Timestamp" && arr[2] == "Message"))
+            {
+                throw new ArgumentException("CSV order does not correspond to the programs expectations. The format should be: 'Author,Timestamp,Message'");
+            }
+
+            int lineno = 1;
+            while((nextln = reader.ReadLine()) != null)
+            {
+                lineno++;
+                arr = nextln.Split(",");
+                if(arr.Length != 3) throw new ArgumentException($"CSV element does not have the correct length at line {lineno}, every line should contain 3 elements");
+                Console.WriteLine($"{arr[1]} - {arr[0]}: {arr[2]}");
+                
+            }
+        }
     }
 
     private static void cheep(string message)
@@ -31,7 +51,7 @@ class Program
         using (var writer = new StreamWriter("chirp_cli_db.csv",true))
         {
             string author = "anon";
-            var timestamp = "100048";
+            var timestamp = DateTime.Now;
             writer.WriteLine($"{author},{timestamp},{message}");
         }
 
