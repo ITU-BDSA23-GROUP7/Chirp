320df6e064c977637a5a2f88be31b4d3a80a28e2
Max <mbko@itu.dk>
2023-10-09 15:47:21 +0200

    CheepServiceUnitTest file.

---
+++ test/Chirp.Razor.Tests/CheepServiceUnitTest.cs
@@ -0,0 +1,18 @@
+namespace Chirp.Razor.Tests;
+
+public class CheepServiceUnitTest
+{
+    [Fact]
+    public void GetCheepsListLength32()
+    {
+        // Arrange
+        ICheepService cheepService = new CheepService
+
+        // Act
+        List<CheepViewModel> cheepList = cheepService.GetCheeps();
+
+
+
+        // Assert
+    }
+}
