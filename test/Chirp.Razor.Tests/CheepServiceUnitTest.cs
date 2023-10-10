b607289e16f222e77c46652bb0f4641ce23de2d8
Max <mbko@itu.dk>
2023-10-10 13:07:43 +0200

    Made unit tests to check if Helge has cheeps and to see if a username that does not exist has cheeps
    Co-authored-by: Casper<ccaassppeerr2411@gmail.com>

--- test/Chirp.Razor.Tests/CheepServiceUnitTest.cs
+++ test/Chirp.Razor.Tests/CheepServiceUnitTest.cs
@@ -15,4 +15,45 @@ public class CheepServiceUnitTest
         // Assert
         Assert.Equal(32, length);
     }
+
+    [Fact]
+    public void GetCheepsFromAuthorHelgeHasMoreThanZeroCheeps()
+    {
+        // Arrange
+        ICheepService cheepService = new CheepService();
+        List<CheepViewModel> helgeCheepList = cheepService.GetCheepsFromAuthor("Helge");
+
+        // Act
+        int length = helgeCheepList.Count;
+
+        // Assert
+        Assert.True(length >= 1, "Helge's list of cheeps did not have a length of 1 or more");
+    }
+
+    [Fact]
+    public void GetCheepsFromAuthorJonradHasNoCheeps()
+    {
+        // Arrange
+        ICheepService cheepService = new CheepService();
+        List<CheepViewModel> jonradCheepList = cheepService.GetCheepsFromAuthor("Jonrad");
+
+        // Act
+        int length = jonradCheepList.Count;
+
+        // Assert
+        Assert.Equal(0, length);
+    }
+
+
+
+    /*public void GetPagesCountFromCheepCountPageCountIs29()
+    {
+        // Arrange
+
+
+        // Act
+
+        // Assert
+
+    }*/
 }
