036c2f5ab8ee87b9eb5d452ea7de4561e39649bf
Casper <124489782+CasperFro@users.noreply.github.com>
2023-10-10 12:32:32 +0200

    Unit test for chirp Razor, checking page length is 32 cheeps.
    Co-authored-by: Max<max@qbrix.dk>

--- test/Chirp.Razor.Tests/CheepServiceUnitTest.cs
+++ test/Chirp.Razor.Tests/CheepServiceUnitTest.cs
@@ -6,15 +6,13 @@ public class CheepServiceUnitTest
     public void GetCheepsListLength32()
     {
         // Arrange
-        ICheepService cheepService = new CheepService;
-        cheepService
-
-        // Act
+        ICheepService cheepService = new CheepService();
         List<CheepViewModel> cheepList = cheepService.GetCheeps();
 
-
-
+        // Act
+        int length = cheepList.Count;
 
         // Assert
+        Assert.Equal(32, length);
     }
 }
--- test/Chirp.Razor.Tests/Usings.cs
+++ test/Chirp.Razor.Tests/Usings.cs
@@ -1,4 +1,1 @@
 global using Xunit;
-global using Chirp.Razor;
-global using System.Net;
-global using Chirp;
