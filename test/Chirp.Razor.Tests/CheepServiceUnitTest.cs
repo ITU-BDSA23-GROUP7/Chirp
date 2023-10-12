4248ae9b3d5d7f749f9da937b1ac2fd32bcb64c1
Daniel Fich <danielfich2@gmail.com>
2023-10-12 15:01:57 +0200

    Corrected tests
    Co-authored-by: Casper <ccaassppeerr2411@gmail.com>
    Co-authored-by: Max <max@qbrix.dk>
    Co-authored-by: Sebastian <hylandersebastian@gmail.com>

--- test/Chirp.Razor.Tests/CheepServiceUnitTest.cs
+++ test/Chirp.Razor.Tests/CheepServiceUnitTest.cs
@@ -3,42 +3,42 @@ namespace Chirp.Razor.Tests;
 public class CheepServiceUnitTest
 {
     [Fact]
-    public void GetCheepsListLength32()
+    public async void GetCheepsListLength32()
     {
         // Arrange
         ICheepService cheepService = new CheepService();
-        List<CheepViewModel> cheepList = cheepService.GetCheeps();
+        IEnumerable<CheepDTO> cheepList = await cheepService.GetCheeps();
 
         // Act
-        int length = cheepList.Count;
+        int length = cheepList.Count();
 
         // Assert
         Assert.Equal(32, length);
     }
 
     [Fact]
-    public void GetCheepsFromAuthorHelgeHasMoreThanZeroCheeps()
+    public async void GetCheepsFromAuthorHelgeHasMoreThanZeroCheeps()
     {
         // Arrange
         ICheepService cheepService = new CheepService();
-        List<CheepViewModel> helgeCheepList = cheepService.GetCheepsFromAuthor("Helge");
+        IEnumerable<CheepDTO> helgeCheepList = await cheepService.GetCheepsFromAuthor("Helge");
 
         // Act
-        int length = helgeCheepList.Count;
+        int length = helgeCheepList.Count();
 
         // Assert
         Assert.True(length >= 1, "Helge's list of cheeps did not have a length of 1 or more");
     }
 
     [Fact]
-    public void GetCheepsFromAuthor6A6F6E726164HasNoCheeps()
+    public async void GetCheepsFromAuthor6A6F6E726164HasNoCheeps()
     {
         // Arrange
         ICheepService cheepService = new CheepService();
-        List<CheepViewModel> cheepList = cheepService.GetCheepsFromAuthor("6A6F6E726164");
+        IEnumerable<CheepDTO> cheepList = await cheepService.GetCheepsFromAuthor("6A6F6E726164");
 
         // Act
-        int length = cheepList.Count;
+        int length = cheepList.Count();
 
         // Assert
         Assert.Equal(0, length);
@@ -67,6 +67,6 @@ public class CheepServiceUnitTest
         int pageCount = cheepService.GetPageCountFromAuthor("6A6F6E726164");
 
         // Assert
-        Assert.True(pageCount == 1, "Page count is not 1.");
+        Assert.True(pageCount == 0, "Page count is not 1.");
     }
 }
