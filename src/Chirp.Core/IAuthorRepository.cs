b601c43e346fb4e48e9007abc5483ff36ea92799
Max <mbko@itu.dk>
2023-11-16 19:56:55 +0100

    Added AuthorDTO

--- src/Chirp.Core/IAuthorRepository.cs
+++ src/Chirp.Core/IAuthorRepository.cs
@@ -1,5 +1,5 @@
 public record AuthorInfo(string Username, string Email);
-
+public record AuthorDTO(int AuthorId, string Name, string Email, IEnumerable<CheepDTO> Cheeps, IEnumerable<AuthorDTO> Following, IEnumerable<AuthorDTO> Followers);
 public interface IAuthorRepository
 {
     Task<AuthorInfo> GetAuthorInfo(string username);
