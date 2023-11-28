bea8a2190df6fae064a022ee70b1b726d08ab273
Casper2411 <112485023+Casper2411@users.noreply.github.com>
2023-11-28 10:57:30 +0100

    Update Author.cs

--- src/Chirp.Infrastructure/Author.cs
+++ src/Chirp.Infrastructure/Author.cs
@@ -2,7 +2,7 @@ public class Author
 {
     public required Guid AuthorId { get; set; }
     public required string Name { get; set; }
-    public required string Email { get; set; }
+    public string? Email { get; set; }
     public List<Cheep> Cheeps { get; set; }
     public List<Author> Following { get; } = new();  //List of people the user follows
     public List<Author> Followers { get;} = new(); //List of people the user is following
