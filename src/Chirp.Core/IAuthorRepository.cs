ef2cdf5d00f377d2868537e38f957837b93ce554
Casper2411 <112485023+Casper2411@users.noreply.github.com>
2023-11-28 10:45:03 +0100

    Update IAuthorRepository.cs

--- src/Chirp.Core/IAuthorRepository.cs
+++ src/Chirp.Core/IAuthorRepository.cs
@@ -2,7 +2,7 @@ namespace Chirp.Core;
 public interface IAuthorRepository
 {
     public Task<AuthorDTO> GetAuthorDTOByUsername(string username);
-    public void CreateNewAuthor(Guid authorId, string name, string email);
+    public void CreateNewAuthor(string name);
 
     public Task FollowAuthor(AuthorDTO authorDTO, AuthorDTO authorToFollowDTO);
     public Task UnfollowAuthor(AuthorDTO authorDTO, AuthorDTO authorToUnfollowDTO);
