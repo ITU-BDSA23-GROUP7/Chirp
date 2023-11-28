058aa2cf96a74b6617c2b5e8380aaf707a4d7d28
Casper2411 <112485023+Casper2411@users.noreply.github.com>
2023-11-28 10:37:49 +0100

    Update IAuthorRepository.cs
    added semicolon

--- src/Chirp.Core/IAuthorRepository.cs
+++ src/Chirp.Core/IAuthorRepository.cs
@@ -6,7 +6,7 @@ public interface IAuthorRepository
 
     public Task FollowAuthor(AuthorDTO authorDTO, AuthorDTO authorToFollowDTO);
     public Task UnfollowAuthor(AuthorDTO authorDTO, AuthorDTO authorToUnfollowDTO);
-    public Task<IEnumerable<string>> GetFollowingUsernames(AuthorDTO authorDTO)
+    public Task<IEnumerable<string>> GetFollowingUsernames(AuthorDTO authorDTO);
 
     Task<bool> UsernameExistsAsync(string username);
     Task CreateNewAuthor(string name);
