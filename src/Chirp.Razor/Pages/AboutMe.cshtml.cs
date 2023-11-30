cb51bdc39c1e2f0107accbbaa694b3670471c00a
Line <linejkpraestegaard@gmail.com>
2023-11-30 14:38:30 +0100

    Added some userinfo
    Co-authored-by: Daniel <danielfich2@gmail.com>

--- src/Chirp.Razor/Pages/AboutMe.cshtml.cs
+++ src/Chirp.Razor/Pages/AboutMe.cshtml.cs
@@ -5,13 +5,39 @@ namespace Chirp.Razor.Pages;
 
 public class AboutMe : PageModel
 {
-    public IActionResult OnGet()
+    private readonly IAuthorRepository _repository;
+    public required AuthorDTO Author { get; set; }
+    public required string Email { get; set; }
+    public required int NumberOfCheeps { get; set; }
+
+
+    public AboutMe(IAuthorRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task SetUserinfo()
+    {
+        Author = await _repository.GetAuthorDTOByUsername(User.Identity.Name);
+
+        Email = Author.Email;
+        Console.WriteLine($"-{Author.Email}-");
+        if (Email == null || Email.Equals(""))
+        {
+            Email = "[No email stored]";
+        }
+        //Cheeps not implementet in repository yet...
+        NumberOfCheeps = 0;
+    }
+
+    public async Task<IActionResult> OnGet()
     {
         if(!User.Identity.IsAuthenticated)
         {
             return Redirect("/");
         }
 
+        await SetUserinfo();
         return null;
     }
 }
