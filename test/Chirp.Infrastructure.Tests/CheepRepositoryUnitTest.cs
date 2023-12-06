25f0393af8645575bb5614110ef04ba44e2abef6
Daniel Fich <danielfich2@gmail.com>
2023-12-06 10:50:55 +0100

    Separated repository tests
    Co-authored-by: Casper <ccaassppeerr2411@gmail.com>

--- test/Chirp.Infrastructure.Tests/CheepRepositoryUnitTest.cs
+++ test/Chirp.Infrastructure.Tests/CheepRepositoryUnitTest.cs
@@ -5,8 +5,6 @@ public class CheepServiceUnitTest
     private readonly ICheepRepository _cheepService;
     private readonly SqliteConnection _connection;
 
-    //This service is used for some tests, but this is not the primary service tested in these tests
-    private readonly IAuthorRepository _authorService;
     public CheepServiceUnitTest()
     {
         //Building the connection to a database
@@ -24,8 +22,6 @@ public class CheepServiceUnitTest
 
         //Making a CheepService from the database.
         _cheepService = new CheepRepository(context);//Here the repository should be created with our context data
-
-        _authorService = new AuthorRepository(context);//And the context is also injected to this service
     }
 
     [Fact]
@@ -113,34 +109,5 @@ public class CheepServiceUnitTest
 
         //Act & Assert
         await Assert.ThrowsAsync<UsernameNotFoundException>(async () => await _cheepService.AddCheepAsync(notExistingName, message));
-    }
-
-    [Fact]
-    public async void HiddenCheepsAreNotShown(){
-        //Arange
-        var username="Casper";
-        await _authorService.CreateNewAuthor(username);
-        await _cheepService.AddCheepAsync(username, "Hello world!!!");
-
-        //Act
-        await _authorService.SetHidden(username, true);
-        IEnumerable<CheepDTO> hiddenUsersCheeps = await _cheepService.GetCheeps(1 , username);
-
-        //Assert
-        Assert.Empty(hiddenUsersCheeps);
-    }
-
-    //Hidden user cannot cheep
-    [Fact]
-    public async void HiddenUserCannotCheep(){
-        //Arrange
-        var hiddenName="Per";
-        await _authorService.CreateNewAuthor(hiddenName);
-
-        //Act
-        await _authorService.SetHidden(hiddenName, true);
-
-        //Assert        
-        await Assert.ThrowsAsync<Exception>(async () => await _cheepService.AddCheepAsync(hiddenName, "Can i as a hidden user cheep?"));
     }
 }
