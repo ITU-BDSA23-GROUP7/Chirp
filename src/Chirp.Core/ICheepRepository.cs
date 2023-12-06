024b1bc9ba33e1475039f38240b3929ec5a6a1f8
Sebastian Cloos Hylander <sehy@itu.dk>
2023-12-06 12:44:27 +0100

    added getFollowingCheep

--- src/Chirp.Core/ICheepRepository.cs
+++ src/Chirp.Core/ICheepRepository.cs
@@ -4,4 +4,5 @@ public interface ICheepRepository
     Task<List<CheepDTO>> GetCheeps(int pageNum = 1, string? author = null);
     int GetPageCount(string? auhtor = null);
     Task AddCheepAsync(string username, string message);
+    Task<List<CheepDTO>> GetFollowerCheeps(string author, int pageNum = 1);
 }
